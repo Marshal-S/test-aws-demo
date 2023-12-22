d88d5fec8a4297de7a32ba5d730cbb40a201f6db
Marshal <>
2023-12-22 15:23:50 +0800

    master

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Upload } from 'antd';
 import './App.css';
+import { requestByUpload } from './request';
 
 function App() {
+
+  const uploadImage = (info: any) => {
+    requestByUpload({
+      "size": info.file.size,
+      "originname": info.file.name,
+      "mimetype": info.file.type,
+    }, info.file)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Upload
+        customRequest={uploadImage}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div>上传图片</div>
+      </Upload>
+    </div >
   );
 }
 
